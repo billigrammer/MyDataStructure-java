22242b648e16f71950fb11bfc014af23e9a936ca
zongzihao <915637831@qq.com>
2018-11-19 21:48:14 +0800

    test

---
+++ 24_BinarySearchTree/BinaryTree.java
@@ -0,0 +1,20 @@
+public class BinaryTree{
+
+    private Node tree;
+
+    public int find(int data){
+        Node p =
+    }
+
+
+
+    public static class Node{
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+}
